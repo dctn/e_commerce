e0ef919dcfe6044777b40bd9bb79f7250a30cb2e
stranger <dctngamers@gmail.com>
2025-04-19 21:49:25 +0530

    restart

--- payment/models.py
+++ payment/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from core.models import Product
 # Create your models here.
 class ShippingAddress(models.Model):
-    address_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
@@ -21,7 +21,7 @@ class ShippingAddress(models.Model):
 
 
 class Order(models.Model):
-    order_models_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     user = models.ForeignKey(User,on_delete=models.RESTRICT)
     address = models.OneToOneField(ShippingAddress,on_delete=models.RESTRICT)
     order_id = models.CharField(max_length=255,null=True,blank=True,editable=False)
