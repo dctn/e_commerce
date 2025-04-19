b76dec0462d7bd8802e0b564cba5bed5ab3e5d8f
stranger <dctngamers@gmail.com>
2025-04-19 20:16:04 +0530

    allowed host and csrf changed

--- e_com_pro/settings.py
+++ e_com_pro/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
-
+# testing git 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
