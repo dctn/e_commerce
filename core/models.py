5fad08e91dcb21f2d6379add53a0df2755fd5494
stranger <dctngamers@gmail.com>
2025-04-19 21:14:50 +0530

    summa

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 # Create your models here.
 class Product(models.Model):
-    product_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     name = models.CharField(max_length=255)
     desp = models.TextField()
     price = models.DecimalField(max_digits=8,decimal_places=0)
