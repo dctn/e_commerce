3eb85b3dbae95815e94ea14bf8fa49dc6b48dc6c
stranger <dctngamers@gmail.com>
2025-04-19 20:35:57 +0530

    changed model and added temp uuid field

--- payment/models.py
+++ payment/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from core.models import Product
 # Create your models here.
 class ShippingAddress(models.Model):
+    temp_uuid = models.UUIDField(default=uuid.uuid4, null=True)
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
